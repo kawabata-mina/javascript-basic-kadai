c388346a54a54f37af856901a0b6602193264ad4
kawabata-mina <minkawab@gmail.com>
2024-09-11 17:28:33 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 定数の宣言
+let userWeight 
+let userHight 
+let bmi
+
+//値の代入
+userWeight = 50;
+userHight = 1.54;
+bmi = ( userWeight / ( userHight * userHight ) )
+
+// コンソールへの出力
+console.log( bmi );
